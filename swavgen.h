94733157732a32f5984947bf0db5015ff6a955cc
ymich9963 <gmichael834@gmail.com>
2025-09-14 23:05:16 +0100

    Fixed welcome string (#2).

--- swavgen.h
+++ swavgen.h
@@ -15,7 +15,7 @@
 #include <string.h>
 #include <time.h>
 
-#define	WELCOME_STR "swavgen : Simple Swavgen Generator\nBy Yiannis Michael (ymich9963).\n\nTo generate your first sound file, you must specify the frequency with '-f' or '--frequency'. This generates a 1 channel, 2 second, 440 Hz sine wave with IEEE-float 64-bit encoding at 48 kHz. Use the '--help' option for a list of all the settings.\n\n" "This program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n"
+#define	WELCOME_STR "swavgen : Simple Swavgen Generator\nBy Yiannis Michael (ymich9963).\n\nTo generate your first sound file, you must specify the wave type with 'swavgen <type>'. This generates a 1 channel, 2 second, 440 Hz wave with IEEE-float 64-bit encoding at 48 kHz. Use the '--help' option for a list of all the settings.\n\n" "This program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n"
 #define	VERSION_STR "swavgen : Version 1.1.3\nBy Yiannis Michael (ymich9963).\n\n"
 
 /* Standard format codes */
